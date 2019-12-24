a111b7de78f94b3135c33cba691e476b68579981
@higakikeita <higaki.keichan@gmail.com>
2019-12-24 17:01:40 +0900

    minor_change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -51,7 +51,7 @@
 # ------------------------------------
 server "18.176.134.115",
    user: "ec2-user",
-   roles: %w{web db app},
+   roles: %w{web db app}
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
