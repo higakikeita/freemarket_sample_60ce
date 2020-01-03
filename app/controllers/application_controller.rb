cd8957a5806019ce05009b648994b768092e2723
@higakikeita <higaki.keichan@gmail.com>
2020-01-03 21:03:00 +0900

    basic_confirm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
-  before_action :basic_auth
+  
 
   private
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '0322'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
