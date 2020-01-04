603967af0a6a4578e23bc949a7362153cea31b1f
Tatsu88-Tokyo <tatsuhiko1028@gmail.com>
2020-01-04 17:09:49 +0900

    header.edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "home#top"
+
+
 end
