c6791d27eb573e64ea17895aa44c95f483bca024
@higakikeita <higaki.keichan@gmail.com>
2020-01-08 17:43:42 +0900

    route-edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#top"
   get "/home/top" => "home#top"
   resources :products, only: [:index]
 end
