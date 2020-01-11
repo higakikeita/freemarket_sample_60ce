980ee5113bd32d16a9ecece8b2a8c022baa81d1c
ayaszmr <s.aya@live.com>
2020-01-11 20:32:21 +0900

    change-to-link-to

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
   root "users#index"
+  resources :user, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
