733fd4e5e99b582ca5776cc88a6aae51c25962b9
ayaszmr <s.aya@live.com>
2020-01-12 16:29:11 +0900

    delete-comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :user, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
