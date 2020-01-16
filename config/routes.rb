e187b2757e4eb9ab806c524e355f2a9d664d9aed
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-01-16 23:17:11 +0900

    クレジットカード登録画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     }
-    devise_scope :user do
-      get 'profiles', to: 'users/registrations#new_phone'
-      get 'addresses', to: 'users/registrations#new_address'
-      post 'addresses', to: 'users/registrations#create_address'
-    end
+  devise_scope :user do
+    get 'sms_authentications', to: 'users/registrations#create_sms_authentication'
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+    get 'credit_cards', to: 'users/registrations#new_credit_card'
+    post 'credit_cards', to: 'users/registrations#create_credit_card'
+  end
   root "home#top"
   resources :users, only: [:index]
   resources :products, only: [:index, :show]
