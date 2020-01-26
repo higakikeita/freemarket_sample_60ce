9ba8c04924c488efbc76295609d28043d04cbe64
vamnasocana <vamnasocana@gmail.com>
2020-01-26 19:03:40 +0900

    コントローラーテスト①

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,1 +1,12 @@
 require 'rails_helper'
+
+describe ProductsController do
+  describe 'GET #new' do
+    it "renders the :new template" do
+    end
+  end
+  describe 'POST #create' do
+    it "renders the :create template" do
+    end
+  end
+end
