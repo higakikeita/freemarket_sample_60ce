bcaf7a48cc149c5d15d1b6962cbc872b8557ea12
vamnasocana <vamnasocana@gmail.com>
2020-01-26 18:28:39 +0900

    modelsのテスト
    バリデーションのチェック

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,10 +1,41 @@
 require 'rails_helper'
 describe Product do  
   describe '#create' do
-    it "is invalid without a nickname" do
-     user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-     user.valid?
-     expect(user.errors[:nickname]).to include("can't be blank")
+    it "全て入力されていればOK" do
+      product = build(:product)
+      expect(product).to be_valid
+    end
+    it "nameが空ならNG" do
+      product = build(:product, name: nil)
+      product.valid?
+      expect(product.errors[:name]).to include("can't be blank")
+    end
+    it "explainが空ならNG" do
+      product = build(:product, explain: nil)
+      product.valid?
+      expect(product.errors[:explain]).to include("can't be blank")
+    end
+    it "priceが空ならNG" do
+      product = build(:product, price: nil)
+      product.valid?
+      expect(product.errors[:price]).to include("can't be blank")
+    end
+    it "priceがinteger以外ならNG" do
+      product = build(:product, price: "３００")
+      product.valid?
+      expect(product.errors[:price]).to include("is not a number")
+    end
+    it "priceが300円未満ならNG" do
+      product = build(:product, price: "290")
+      product.valid?
+      expect(product.errors[:price]).to include("must be greater than or equal to 300")
+    end
+    it "priceが9999999円よりも高額ならNG" do
+      product = build(:product, price: "19999999")
+      product.valid?
+      expect(product.errors[:price]).to include("must be less than or equal to 9999999")
     end
   end
 end
+
+
