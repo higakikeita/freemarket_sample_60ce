270f4e1e365b5fc184ded3b28ed33ab172099f94
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-01-31 19:30:51 +0900

    インスタンス変数と部分テンプレートの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show,:comment,:edit,:update]
   def index
     @ladies = Product.where(category_id: "1").order(created_at: "DESC").limit(10)
-    @mens = Product.where(category_id: "2").order(created_at: "DESC").limit(10)
+    @men = Product.where(category_id: "2").order(created_at: "DESC").limit(10)
     @home_appliances = Product.where(category_id: "8").order(created_at: "DESC").limit(10)
     @hobbies = Product.where(category_id: "6").order(created_at: "DESC").limit(10)
   end
