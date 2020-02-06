749433a5a3a1b37f13155a528f5d7cf325bba932
@higakikeita <higaki.keichan@gmail.com>
2020-02-06 19:18:51 +0900

    create-ajax#1

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,16 @@
+$(function () {
+  $('#new_comment').on('submit', function (e) {
+    console.log('hoge');
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
+});
