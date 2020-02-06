de7c3f43d156d4703949948d2f6f8173ca97fb3b
@higakikeita <higaki.keichan@gmail.com>
2020-02-06 23:58:27 +0900

    comment-user_set

--- db/migrate/20200125120245_create_comments.rb
+++ db/migrate/20200125120245_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
     create_table :comments do |t|
       t.text     :comment
       t.references :product,foreign_key:true
+      t.references :user,foreign_key:true
       t.timestamps
     end
   end
