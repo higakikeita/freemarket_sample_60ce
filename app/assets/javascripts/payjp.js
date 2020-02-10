dcb94508436dc830bb643c8a629a2bf3104dcf5c
Tatsu88-Tokyo <tatsuhiko1028@gmail.com>
2020-02-10 15:36:22 +0900

    コメントアウト・デバッガーの削除

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -2,19 +2,15 @@ $(function() {
   Payjp.setPublicKey('pk_test_57c5bfaa1f1d1f2acd058a77');
   $("#charge-form").on('click', function(e){
     e.preventDefault();
-    // form.find("input[type=submit]").prop("disabled", true);
     let card = {
         number: $('#creditcard_card_number').val(),
         cvc:$('#creditcard_card_pass').val(),
         exp_month: $('#creditcard_card_month').val(),
         exp_year: $('#creditcard_card_year').val()
     };
-    console.log(card);
 
     Payjp.createToken(card, function(status, response) {
-      console.log(status);
       if (response.error) {
-
         $("#charge-form").prop('disabled', false);
         alert("カード情報が正しくありません。");
       }
@@ -23,12 +19,9 @@ $(function() {
         $(".cvc").removeAttr("name");
         $(".exp_month").removeAttr("name");
         $(".exp_year").removeAttr("name");
-
         let token = response.id;
-        console.log(token);
         $("#card_token").append(`<input type="hidden" name="payjpToken" value=${token}>`);
         $("#form").get(0).submit();
-        console.log($("#charge-form"));
         alert("登録が完了しました");
       }
     });
