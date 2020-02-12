ccf8214ab65a61a3d58cf1c5b48892a2e115482d
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-02-13 00:33:43 +0900

    TOPページ_商品ブランドの実装

--- app/assets/javascripts/nav-list.js
+++ app/assets/javascripts/nav-list.js
@@ -1,3 +1,4 @@
+// ナビゲーションバー（カテゴリー）
 $(function() {
 	$(".header__contents__below__search__category").hover(function() {
 		$("ul.level1").toggle();
@@ -8,6 +9,17 @@ $(function() {
 		$(">a", this).addClass("active");
 	}, function() {
 		$(">ul:not(:animated)", this).stop(true, true).slideUp("fast");
+		$(">a", this).removeClass("active");
+	});
+});
+// ナビゲーションバー（ブランド）
+$(function() {
+	$(".header__contents__below__search__brand").hover(function() {
+		$("ul.level1--category").toggle();
+	});
+	$(".header__contents__below__search__brand li").hover(function() {
+		$(">a", this).addClass("active");
+	}, function() {
 		$(">a", this).removeClass("active");
 	});
 });
