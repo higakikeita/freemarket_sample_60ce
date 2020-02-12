af5e0fae2399f1bc083955e9d140e18575705362
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-02-12 20:50:31 +0900

    nav-list.js_インデントの修正

--- app/assets/javascripts/nav-list.js
+++ app/assets/javascripts/nav-list.js
@@ -1,14 +1,13 @@
 $(function() {
 	$(".header__contents__below__search__category").hover(function() {
-    $("ul.level1").toggle();
-  });
+		$("ul.level1").toggle();
+	});
 	$(".header__contents__below__search__category li ul").hide();
 	$(".header__contents__below__search__category li").hover(function() {
-    $(">ul:not(:animated)", this).stop(true, true).slideDown("fast");
+		$(">ul:not(:animated)", this).stop(true, true).slideDown("fast");
 		$(">a", this).addClass("active");
-	},
-	function() {
+	}, function() {
 		$(">ul:not(:animated)", this).stop(true, true).slideUp("fast");
-    $(">a", this).removeClass("active");
-  });
+		$(">a", this).removeClass("active");
+	});
 });
