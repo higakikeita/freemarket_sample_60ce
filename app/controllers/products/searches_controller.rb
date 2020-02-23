166fa704d6f2c03e4e19b8bbb72edd42e8f10bbc
@higakikeita <higaki.keichan@gmail.com>
2020-02-23 21:24:05 +0900

    コントローラ追加

--- app/controllers/products/searches_controller.rb
+++ app/controllers/products/searches_controller.rb
@@ -2,5 +2,7 @@ class Products::SearchesController < ApplicationController
   def index
     @products = Product.search(params[:keyword]).order("id DESC")
     @keyword = params[:keyword]
+    @search = Product.search(params[:q])
+    # @products = @search.result
   end
 end
