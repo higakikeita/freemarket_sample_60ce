a038febb7ef041ff2c5f7a3a3f7a4c83aedf1887
@higakikeita <higaki.keichan@gmail.com>
2020-02-25 19:48:07 +0900

    privateメソッド設定

--- app/controllers/products/searches_controller.rb
+++ app/controllers/products/searches_controller.rb
@@ -1,7 +1,15 @@
 class Products::SearchesController < ApplicationController
+  before_action :set_ransack
   def index
     @products = Product.search(params[:keyword]).order("id DESC")
     @keyword = search_params[:name_cont]
     @products = @q.result(distinct: true)
+  end
+  private
+  def set_ransack
+    @q = Product.ransack(params[:q])
+  end
+  def search_params
+    params.require(:q).permit(:search_order,:name_cont,:brand_cont,:size_id_eq,:status_id_eq,:shipping_fee_id_eq,:purchase_status_id_eq,:category_grandparent_id_eq,:category_parent_id_eq,:category_id_eq,:price_lteq,:price_gteq)
   end
 end
