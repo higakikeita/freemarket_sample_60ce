e1c20c08b39d9449c2f1e4f4aa7ab954239c5520
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-03-03 23:58:37 +0900

    タイムセール機能の実装（＊開発環境のみ）-R

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = Rails.env.to_sym
 rails_root = Rails.root.to_s
 
-# environment は設定しないと production になってしまう
 set :environment, rails_env
 set :output, "#{rails_root}/log/cron.log"
 
